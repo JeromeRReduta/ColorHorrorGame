85a3a173af231895f68cd7d6b54d6324d19a46b4
Maleke Hanson <mchanson3@dons.usfca.edu>
2021-07-07 16:50:42 -0400

    Movement Script Test

--- ColorHorror/Assets/Scipts/PlayerMovement.cs
+++ ColorHorror/Assets/Scipts/PlayerMovement.cs
@@ -9,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
-    public Animator animator;
+    //public Animator animator;
     Vector2 movement;
     void Update()
     {
