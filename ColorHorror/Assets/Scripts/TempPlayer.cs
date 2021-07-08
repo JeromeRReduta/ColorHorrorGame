8e4eafc4db94f05efd72500ca7e47438f6751e2c
Adrian Silagan <acsilagan@usfca.edu>
2021-07-08 16:59:34 -0500

    added the test stage

---
+++ ColorHorror/Assets/Scripts/TempPlayer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayer : MonoBehaviour
+{
+
+    public static TempPlayer Instance;
+    [SerializeField] private float CharacterSpeed = 1.0f;
+    [HideInInspector] public Vector2 InputDir;
+    
+    public Rigidbody2D Playerbody;
+    [HideInInspector] public float VertInput;
+    [HideInInspector] public float HorInput;
+
+    void Start()
+    {
+        Instance = this;
+    }
+    
+    void Update()
+    {
+        VertInput = Input.GetAxis("Vertical");
+        HorInput = Input.GetAxis("Horizontal");
+        InputDir = new Vector2(HorInput, VertInput).normalized;
+        Camera.main.transform.position = new Vector3 (Playerbody.transform.position.x, Playerbody.transform.position.y, Camera.main.transform.position.z);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 WhereAmI = Playerbody.position;
+        Vector2 WhereTo = WhereAmI + (InputDir * CharacterSpeed) * Time.fixedDeltaTime;
+        Playerbody.MovePosition(WhereTo);
+    }
+}
