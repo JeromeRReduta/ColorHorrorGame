c8f9ade2df42db67a7797871447b5b738c323622
Maleke Hanson <mchanson3@dons.usfca.edu>
2021-07-18 17:29:09 -0400

    Update PlayerCollsion.cs

--- ColorHorror/Assets/PlayerCollsion.cs
+++ ColorHorror/Assets/PlayerCollsion.cs
@@ -8,7 +8,7 @@ public class PlayerCollsion : MonoBehaviour
 
     public GameObject m_GotHitScreen;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "enemy")
         {
