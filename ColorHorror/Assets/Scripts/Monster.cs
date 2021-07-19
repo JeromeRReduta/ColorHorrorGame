95cbe6086a20470e8ad471b7c362f43e5357b578
JeromeRReduta <38510370+JeromeRReduta@users.noreply.github.com>
2021-07-19 16:59:35 -0700

    Used OOP for Monsters & Red White Monsters

--- ColorHorror/Assets/Scripts/Monster.cs
+++ ColorHorror/Assets/Scripts/Monster.cs
@@ -5,28 +5,41 @@ using Pathfinding;
 
 /**
 Base code for all monsters
+
+Every monster has the following properties:
+1) Collider2D
+2) Rigidbody2D
+
+Every monster has the following behavior:
+1) Moves towards players - handled by a* algorithm
+2) Whenever it hits anything, stops. Whenever it hits the player, attempts to damage player.
 */
-public class Monster : MonoBehaviour
+public abstract class Monster : MonoBehaviour
 {
     // Note: Public typeName X {get; private set;}
     // means we can call X's value from any other class (implied public get), but can only set its value in this class (private set)
 
+    /** This monster's collider */
     public Collider2D Col {get; private set;}
+
+    /** This monster's rigid body */
     public Rigidbody2D Rb {get; private set;}
 
-    public virtual void Start()
+    /** Start */
+    public void Start()
     {
         Col = GetComponent<Collider2D>();
         Rb = GetComponent<Rigidbody2D>();
     }
 
-    public virtual void Update()
-    {
-        // Path handles the movement logic, so don't have to do anything here
-    }
-
-    // Need OnCollisionEnter2D for 2d objects, not OnCollisionEnter
-    void OnCollisionEnter2D(Collision2D collision)
+    /** Update func */
+    public abstract void Update();
+    
+    /**
+    Collision logic. On collision with anything, the monster stops. On collision with player, attempts to deal damage to player.
+    @param collision Collision data
+    */
+    public void OnCollisionEnter2D(Collision2D collision)
     {
         Rb.velocity = new Vector2(0f, 0f);
         Debug.Log(collision);
@@ -38,5 +51,4 @@ public class Monster : MonoBehaviour
         }
         
     }
-
 }
