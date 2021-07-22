fdc1512a2346bb7086ddee506b0f93f948ea1fb2
Maleke Hanson <mchanson3@dons.usfca.edu>
2021-07-22 18:54:41 -0400

    Making Scene TP

---
+++ ColorHorror/Assets/Scripts/TPTrigger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    void OnTriggerEnter2D()
+    {
+        gameManager.CompleteLevel();
+    }
+}
