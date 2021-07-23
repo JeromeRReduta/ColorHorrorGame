52bd9906209dc5b61dc8fcb09849da7b5bd2ec20
Maleke Hanson <mchanson3@dons.usfca.edu>
2021-07-22 20:26:32 -0400

    Debugging Scripts

--- ColorHorror/Assets/Scripts/GameManager.cs
+++ ColorHorror/Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    LevelLoader levelLoader;
+public LevelLoader levelLoader;
    public void CompleteLevel ()
    {
        levelLoader.LoadNextLevel();
--- ColorHorror/Assets/Scripts/TPTrigger.cs
+++ ColorHorror/Assets/Scripts/TPTrigger.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TPTrigger : MonoBehaviour
 {
+    LevelLoader levelLoader;
     public GameManager gameManager;
     void OnTriggerEnter2D()
     {
