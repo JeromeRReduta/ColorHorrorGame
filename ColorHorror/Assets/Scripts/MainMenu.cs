04903abc852aef8389c33af89d55654ade668424
Maleke Hanson <mchanson3@dons.usfca.edu>
2021-07-22 20:37:23 -0400

    Testing Transitions For Main Menu buttons

--- ColorHorror/Assets/Scripts/MainMenu.cs
+++ ColorHorror/Assets/Scripts/MainMenu.cs
@@ -5,8 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    public GameManager gameManager;
     public void StartGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        gameManager.CompleteLevel();
     }
 }
