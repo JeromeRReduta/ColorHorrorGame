1346fdf6fb0d1ad2d213f7f6d131381be00b89d4
Maleke Hanson <mchanson3@dons.usfca.edu>
2021-07-29 16:45:53 -0400

    Testing Disabling A.I pathing

--- ColorHorror/Assets/ChangePlayerColor.cs
+++ ColorHorror/Assets/ChangePlayerColor.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class ChangePlayerColor : MonoBehaviour
 {
     public float paintTime = 3f;
+    public AIPath Path;
+
     public PolygonCollider2D redPool;
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -18,9 +21,11 @@ public class ChangePlayerColor : MonoBehaviour
     {
         transform.GetComponent<Renderer>().material.color = new Color(0,0,0);
         Debug.Log("HIT RED POOL");
+        Path.enabled = false;
         yield return new WaitForSeconds(paintTime);
         transform.GetComponent<Renderer>().material.color = new Color(255,255,255);
         Debug.Log("I AM NO LONGER RED");
+        Path.enabled = true;
     }
 
 
