f951aae468274b6d10a1cb8b385edc491e15af57
Maleke Hanson <mchanson3@dons.usfca.edu>
2021-07-29 17:39:21 -0400

    Changing Script to include actual RedMonster

--- ColorHorror/Assets/ChangePlayerColor.cs
+++ ColorHorror/Assets/ChangePlayerColor.cs
@@ -7,7 +7,7 @@ public class ChangePlayerColor : MonoBehaviour
 {
     public float paintTime = 10f;
     public AIPath Path;
-    //public GameObject RedMonster;
+    public GameObject RedMonster;
 
     public PolygonCollider2D redPool;
     private void OnTriggerEnter2D(Collider2D col)
@@ -24,13 +24,13 @@ public class ChangePlayerColor : MonoBehaviour
         Debug.Log("HIT RED POOL");
         //Path.enabled = false;
 
-        this.GetComponent<RedMonsterNew>().enabled = false;
+        RedMonster.GetComponent<RedMonsterNew>().enabled = false;
 
         yield return new WaitForSeconds(paintTime);
         transform.GetComponent<Renderer>().material.color = new Color(255,255,255);
         Debug.Log("I AM NO LONGER RED");
         
-        this.GetComponent<RedMonsterNew>().enabled = true;
+        RedMonster.GetComponent<RedMonsterNew>().enabled = true;
         //Path.enabled = true;
     }
 
