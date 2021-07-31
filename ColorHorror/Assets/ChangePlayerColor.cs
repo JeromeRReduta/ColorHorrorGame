f22f4690f35a4fc457ccb0a3bba0ca6f871324b9
Maleke Hanson <mchanson3@dons.usfca.edu>
2021-07-31 15:42:13 -0400

    Adding the two other monster's to the script

--- ColorHorror/Assets/ChangePlayerColor.cs
+++ ColorHorror/Assets/ChangePlayerColor.cs
@@ -6,8 +6,10 @@ using Pathfinding;
 public class ChangePlayerColor : MonoBehaviour
 {
     public float paintTime = 10f;
-    public AIPath Path;
+    //public AIPath Path;
     public GameObject RedMonster;
+    //public GameObject BlueMonster;
+    //public GameObject YellowMonster;
 
     public PolygonCollider2D redPool;
     private void OnTriggerEnter2D(Collider2D col)
@@ -15,12 +17,20 @@ public class ChangePlayerColor : MonoBehaviour
         if(col.gameObject.tag == "RedPool"){
            StartCoroutine(ChangeColorRed());
         }
+
+        if(col.gameObject.tag == "BluePool"){
+           StartCoroutine(ChangeColorBlue());
+        }
+
+        if(col.gameObject.tag == "YellowPool"){
+           StartCoroutine(ChangeColorYellow());
+        }
         
     }
 
     IEnumerator ChangeColorRed()
     {
-        transform.GetComponent<Renderer>().material.color = new Color(0,0,0);
+        transform.GetComponent<Renderer>().material.color = new Color(176,0,0);
         Debug.Log("HIT RED POOL");
         //Path.enabled = false;
 
@@ -31,6 +41,38 @@ public class ChangePlayerColor : MonoBehaviour
         Debug.Log("I AM NO LONGER RED");
         
         RedMonster.GetComponent<RedMonsterNew>().enabled = true;
+        //Path.enabled = true;
+    }
+
+    IEnumerator ChangeColorBlue()
+    {
+        transform.GetComponent<Renderer>().material.color = new Color(0,189,255);
+        Debug.Log("HIT BLUE POOL");
+
+        //Path.enabled = false;  //Figure out how to disable AI path
+        //BlueMonster.GetComponent<RedMonsterNew>().enabled = false; //Would have to disable the Monster script due to how the scripts work////
+
+        yield return new WaitForSeconds(paintTime);
+        transform.GetComponent<Renderer>().material.color = new Color(255,255,255);
+        Debug.Log("I AM NO LONGER BLUE");
+        
+        //BlueMonster.GetComponent<RedMonsterNew>().enabled = true;
+        //Path.enabled = true;
+    }
+
+    IEnumerator ChangeColorYellow()
+    {
+        transform.GetComponent<Renderer>().material.color = new Color(233,236,0); //Figure out color change
+        Debug.Log("HIT YELLOW POOL");
+
+        //Path.enabled = false;          //Figure out how to disable AI path
+        //BlueMonster.GetComponent<RedMonsterNew>().enabled = false; //Would have to disable the Monster script due to how the scripts work////
+
+        yield return new WaitForSeconds(paintTime);
+        transform.GetComponent<Renderer>().material.color = new Color(255,255,255); //Figure out color change
+        Debug.Log("I AM NO LONGER YELLOW");
+        
+        //BlueMonster.GetComponent<RedMonsterNew>().enabled = true;
         //Path.enabled = true;
     }
 
